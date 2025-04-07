e140b43a48b5dcd3b26550ed5e1cc07081e27eec
twamp22 <wright.thomas@live.com>
2025-04-06 21:58:34 -0700

    Removed EFT and added Arena

--- eft-dma-radar/Tarkov/Features/MemoryWrites/RageMode.cs
+++
@@ -1,9 +0,0 @@
-﻿using eft_dma_shared.Common.Features;
-
-namespace eft_dma_radar.Tarkov.Features.MemoryWrites
-{
-    public sealed class RageMode : MemWriteFeature<RageMode>
-    {
-        /// Container to set the Enabled property. Uses default implementation.
-    }
-}
